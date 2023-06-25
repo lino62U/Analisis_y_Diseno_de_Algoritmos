617c1a8124e9134f6f3eb385cb62e2bf9282d9dc
Avelino Lupo <103952759+lino62U@users.noreply.github.com>
2023-06-25 17:47:49 -0500

    Update problema_del_viajante_vertices.cpp

--- TAREA_05/Vertice_AgenteViajero/problema_del_viajante_vertices.cpp
+++ TAREA_05/Vertice_AgenteViajero/problema_del_viajante_vertices.cpp
@@ -5,7 +5,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
 #include <list>
 #include <algorithm>
 #include <string>
@@ -13,334 +12,248 @@ using namespace std;
 
 #define INF 99999
 
-
 using namespace std;
 
-
-template<class V, class A>
+template <class V, class A>
 class Edge;
 
-
-
-
-template<class V,class A>
+template <class V, class A>
 class Vertex
 {
-    public:
-     V                   m_Dato;
-     int                 m_Costo;
-     bool                m_Visit; 
-     list<Edge<V,A> >    m_list_Edge;
-  
+public:
+    V m_Dato;
+    int m_Costo;
+    bool m_Visit;
+    list<Edge<V, A>> m_list_Edge;
+
     Vertex(V vertice)
     {
         m_Dato = vertice;
         m_Costo = INF;
         m_Visit = false;
-
     }
 
-    bool operator==(const Vertex<V,A> & Ve)
+    bool operator==(const Vertex<V, A> &Ve)
     {
         return m_Dato == Ve.m_Dato;
     }
-
 };
 
-
-
-template<class V,class A>
+template <class V, class A>
 class Edge
 {
-    public:
-        A                   m_Dato;
-        Vertex<V,A>  *      m_pVertex;
+public:
+    A m_Dato;
+    Vertex<V, A> *m_pVertex;
 
-        Edge(A a, Vertex<V,A>  *q=0)
-        {
-            m_Dato = a;
-            m_pVertex=q;
-        }
+    Edge(A a, Vertex<V, A> *q = 0)
+    {
+        m_Dato = a;
+        m_pVertex = q;
+    }
 
-
-    bool operator==(const Edge<V,A> & Ar)
+    bool operator==(const Edge<V, A> &Ar)
     {
         return m_Dato == Ar.m_Dato;
     }
-
 };
 
-
-
-template<class V,class A>
+template <class V, class A>
 class Graph
 {
-   typedef typename   list< Vertex <V,A>>::iterator Iterator_Vertex; 
-   typedef typename   list<  Edge  <V,A>>::iterator Iterator_Edge;  
-   private:
-      list< Vertex <V,A> >   m_Graph;
-   public:
-   Graph(){};
+    typedef typename list<Vertex<V, A>>::iterator Iterator_Vertex;
+    typedef typename list<Edge<V, A>>::iterator Iterator_Edge;
 
-   void Insert(V Vertice)
-   {
-          m_Graph.push_back(Vertex<V,A>(Vertice));
-   };
-   void Insert_Edge(V v1, V v2, A a)
-   {
-         Iterator_Vertex p = find_Vertex(v1);
-         Iterator_Vertex q = find_Vertex(v2);
-         if(p!= m_Graph.end() && q!= m_Graph.end())
-         { 
+private:
+    list<Vertex<V, A>> m_Graph;
+
+public:
+    Graph(){};
+
+    void Insert(V Vertice)
+    {
+        m_Graph.push_back(Vertex<V, A>(Vertice));
+    };
+    void Insert_Edge(V v1, V v2, A a)
+    {
+        Iterator_Vertex p = find_Vertex(v1);
+        Iterator_Vertex q = find_Vertex(v2);
+        if (p != m_Graph.end() && q != m_Graph.end())
+        {
             // ¿Qué ocurre si la arista y existe?
-            Vertex<V,A>* pDestino = &(*q); 
-            p->m_list_Edge.push_back(Edge<V,A>(a, pDestino));
-         }
-          
-   };
+            Vertex<V, A> *pDestino = &(*q);
+            p->m_list_Edge.push_back(Edge<V, A>(a, pDestino));
+        }
+    };
 
-   void Reset_Graph()
-   {
-     Iterator_Vertex it = m_Graph.begin();
-       for(; it != m_Graph.end();it++)
-       {
-          it->m_Visit = false;
-       }
-   }
-
-   void DFS()
-   {
-       stack< Iterator_Vertex > pila;
-       pila.push(m_Graph.begin());
-       while (!pila.empty())
-       {
-           Iterator_Vertex it = pila.top();
-           pila.pop();
-           if(!it->m_Visit)
-           {
-                it->m_Visit=true;
-                cout<<it->m_Dato<<endl;
-           }
-           Iterator_Edge it_e = it->m_list_Edge.begin();
-           for(; it_e != it->m_list_Edge.end();++it_e)
-           {
-               if(it_e->m_pVertex->m_Visit==false)
-               {
-                Iterator_Vertex it_d = find_Vertex(it_e->m_pVertex->m_Dato);
-                if(it_d != m_Graph.end())
-                  pila.push(it_d);
-               }
-           }
-       }
-       
-   }
-
-    void BFS()
-   {
-       Reset_Graph();
-       queue< Iterator_Vertex > cola;
-       cola.push(m_Graph.begin());
-       while (!cola.empty())
-       {
-           Iterator_Vertex it = cola.front();
-           cola.pop();
-           
-           if(!it->m_Visit)
-           {
-                it->m_Visit=true;
-                cout<<it->m_Dato<<endl;
-           }
-           Iterator_Edge it_e = it->m_list_Edge.begin();
-           for(; it_e != it->m_list_Edge.end();++it_e)
-           {
-               Iterator_Vertex it_d = find_Vertex(it_e->m_pVertex->m_Dato);
-                if(it_d != m_Graph.end() && it_d->m_Visit==false)
-                    cola.push(it_d);
-               
-           }
-       }
-       
-   }
-
-   Iterator_Vertex  find_Vertex(V v)
-   {
-    return    std::find(m_Graph.begin(), m_Graph.end(), Vertex<V,A>(v));
-   } 
-     
-   
-   bool find_edge(V v, A a)
-   {
-        Iterator_Vertex  p = find_Vertex(v);
-        Iterator_Edge    q = std::find(p->m_list_Edge.begin(), p->m_list_Edge.end(), Edge<V,A>(a));
-        if(q!=p->m_list_Edge.end())
-           return true;   
-   }
-   void Print()
-   {
-       typedef typename   list< Vertex <V,A>>::iterator Iterator_Vertex; 
-       Iterator_Vertex it = m_Graph.begin();
-       for(; it != m_Graph.end();it++)
-       {
-          Iterator_Edge    it_e = it->m_list_Edge.begin();
-          for(; it_e != it->m_list_Edge.end();++it_e) 
-           { 
-              cout<<it->m_Dato<<" ---> "<<it_e->m_Dato<<"---->"<<it_e->m_pVertex->m_Dato<<endl;
-           }
-          
-       }
-
-    }
-    
-    int mini_dis(vector<int> &d)
+    void Reset_Graph()
     {
         Iterator_Vertex it = m_Graph.begin();
-        int min;
-        int i=0;
-     
-        for(; it != m_Graph.end();it++)
+        for (; it != m_Graph.end(); it++)
         {
-            if(!it->m_Visit)
-            {
-                min = d[i];
-                break;   
-            }
-            i++;
+            it->m_Visit = false;
         }
-        i=0;
-        it = m_Graph.begin();
-        for(; it != m_Graph.end();it++)
-        {
-            if(!it->m_Visit && min > it->m_Costo)
-                min = d[i];
-            i++;
-        }
-
-        return min;
-        
     }
-   
-    
-    void disjtrak_alg(V Or)
+
+    Iterator_Vertex find_Vertex(V v)
     {
-        typedef priority_queue <int, vector<int>, greater<int> > min_heap;
-        typedef Vertex<string,int> vertesss;
-        Iterator_Vertex O = find_Vertex(Or);
-        O->m_Costo=0;
-	    //min_heap d;
-	    int n_vectors =m_Graph.size() ;
-        vector<int> d(n_vectors, 0);
-        vector<string> previus(n_vectors);
-        
-        //_________________________________________________________________________________________________
-        
-        for(int m=0; m<n_vectors;m++)
+        return std::find(m_Graph.begin(), m_Graph.end(), Vertex<V, A>(v));
+    }
+
+    bool find_edge(V v, A a)
+    {
+        Iterator_Vertex p = find_Vertex(v);
+        Iterator_Edge q = std::find(p->m_list_Edge.begin(), p->m_list_Edge.end(), Edge<V, A>(a));
+        if (q != p->m_list_Edge.end())
+            return true;
+    }
+    void Print()
+    {
+        typedef typename list<Vertex<V, A>>::iterator Iterator_Vertex;
+        Iterator_Vertex it = m_Graph.begin();
+        for (; it != m_Graph.end(); it++)
         {
-            vector<Edge<string,int>*> lst_edges;
-            for(Iterator_Edge or_ed = O->m_list_Edge.begin(); or_ed != O->m_list_Edge.end();++or_ed) 
-                lst_edges.push_back(&(*or_ed));
-            
-            O->m_Visit=true;
-            
-            int k =0;
-            for (Iterator_Vertex it = m_Graph.begin();it!=m_Graph.end(); it++)
+            Iterator_Edge it_e = it->m_list_Edge.begin();
+            for (; it_e != it->m_list_Edge.end(); ++it_e)
             {
-                if(it->m_Visit)
-                {
-                    d[k] = it->m_Costo;
-                }
-                else
-                {
-                    bool band=false;
-                    for(auto i =lst_edges.begin(); i != lst_edges.end();++i) 
-                    { 
-                        if((*i)->m_pVertex == &(*it))
-                        {
-                            if( d[k] != (std::min(O->m_Costo + (*i)->m_Dato, it->m_Costo)))
-                                previus[k] = O->m_Dato;
-                            d[k] = std::min(O->m_Costo + (*i)->m_Dato, it->m_Costo);
-                            
-                            band=true;
-                            it->m_Costo = std::min(O->m_Costo + (*i)->m_Dato, it->m_Costo)  ;
-                            lst_edges.erase(i);
-                            break;
-                        }
-                    }
-                    if(!band)
-                        d[k] = it->m_Costo;
-                }
-                k++;
+                cout << it->m_Dato << " ---> " << it_e->m_Dato << "---->" << it_e->m_pVertex->m_Dato << endl;
             }
-            lst_edges.clear();
-            
-            if(m==0){
-                O->m_Costo= d[0]=INF;
-            }
-            
-            int peso = mini_dis(d);
-
-            Iterator_Vertex ittt = m_Graph.begin();
-            vector<int>::iterator it_vd = d.begin();
-            
-            for(; ittt != m_Graph.end();ittt++)
-            {
-                if((*it_vd) ==peso && !ittt->m_Visit){
-                    O = ittt;
-                    break;
-                }
- 
-                it_vd++;
-            }
-            k=0;
-            
         }
-        
-        
-        
-        cout<<"\n\ndistanaci: "<<d.size()<<endl;
-
-        for(int l=0;l<d.size();l++)
-            cout<<d[l]<< "\t\t: " <<previus[l]<<endl;
-
     }
 
 
+    void heuristicaVecinoMasCercano(V Or, vector<Vertex<V,A> *> &solucion)
+    {
+        
 
-   
+        Vertex<V,A> * nodoActual = &(*find_Vertex(Or));
+        
+        solucion.push_back( &(*nodoActual));
+        nodoActual->m_Visit = true;
+        
+        Vertex<V,A> * nodoSiguiente = nodoActual->m_list_Edge.begin()->m_pVertex ;
+         int min_d;
+        while (solucion.size() < m_Graph.size())
+        {
+            //&(*it)
+            
 
+            min_d = INF;
+            // Buscar el nodo no vistado mas cercano al actual
+            //min_element(nodoActual->m_list_Edge.begin(), nodoActual->m_list_Edge.end(), [](const Iterator_Edge & a, Iterator_Edge & b) { return (*a)->m_Dato })
+            for (Iterator_Edge it = nodoActual->m_list_Edge.begin();  it != nodoActual->m_list_Edge.end(); it++)
+            {
+                if(!it->m_pVertex->m_Visit &&  it->m_Dato < min_d)
+                {
+                    nodoSiguiente = it->m_pVertex;
+                    min_d=it->m_Dato;
+                }
+            }
+
+            // Si se encontró un nodo no visitado, agregarlo al recorrido y marcarlo como visitado
+            if (nodoSiguiente) {
+                
+                
+                solucion.push_back(nodoSiguiente); 
+                nodoSiguiente->m_Visit = true;
+                nodoSiguiente->m_Costo = min_d;
+                nodoActual = nodoSiguiente;
+            }
+            
+            
+        }
+        
+        for (Iterator_Edge it = nodoSiguiente->m_list_Edge.begin();  it != nodoSiguiente->m_list_Edge.end(); it++)
+        {
+            if(it->m_pVertex->m_Dato == solucion[0]->m_Dato)
+            {
+                solucion[0]->m_Costo = it->m_Dato;
+                break;
+            }
+        }
+        
+
+    }
+
+    
 };
-
-
+template <class V, class A>
+void printSolution(vector<Vertex<V,A> *>& solucion )
+{
+    A costoTotal = 0;
+    cout<<"\nNodo inicial: "<<solucion[0]->m_Dato<<endl;
+    for(auto k: solucion)
+    {
+        cout<<k->m_Dato<<" -> ";
+        costoTotal+=  k->m_Costo;
+    }
+    cout<<solucion[0]->m_Dato<<endl;
+    
+    bool band = false;
+    for(auto k: solucion)
+    {
+        if(band)
+        {
+            cout<<"  "<<k->m_Costo<<" ";
+            
+        }
+        band=true;
+        costoTotal+=  k->m_Costo;
+    }
+    cout<<"  "<<solucion[0]->m_Costo<<endl;
+    
+    cout<<"\nCosto Total: "<<costoTotal<<endl<<endl;
+}
 void test1()
 {
-   Graph<string, int>  G;
-   G.Insert("Arequipa");
-   G.Insert("Lima");
-   G.Insert("Cusco");
-   G.Insert("Puno");
-   G.Insert("Tacna");
-   G.Insert("Ica");
+    Graph<string, int> G;
+    std::vector<Vertex<string,int> *> solucion;
+    
+    
+    G.Insert("1");
+    G.Insert("2");
+    G.Insert("3");
+    G.Insert("4");
+    G.Insert("5");
 
-   G.Insert_Edge("Arequipa","Cusco",100);
-   G.Insert_Edge("Arequipa","Puno",160);
-   G.Insert_Edge("Arequipa","Lima",1500);
-   G.Insert_Edge("Puno","Ica",1500);
-   G.Insert_Edge("Puno","Tacna",1070);
-   G.Insert_Edge("Cusco","Puno",400);
-   G.Insert_Edge("Cusco","Tacna",600);
-   G.Insert_Edge("Tacna","Lima",600);
-   G.Insert_Edge("Tacna","Arequipa",600);
-   G.Insert_Edge("Lima","Puno",600);
-   G.Insert_Edge("Ica","Arequipa",600);
-   
-   
-   
+    G.Insert_Edge("5","1", 45);
+    G.Insert_Edge("5","2", 40);
+    G.Insert_Edge("5","3", 30);
+    G.Insert_Edge("5","4", 50);
 
-   //G.Print();
-   //G.BFS();
-   G.disjtrak_alg("Arequipa");
+    G.Insert_Edge("1","2", 10);
+    G.Insert_Edge("1","3", 55);
+    G.Insert_Edge("1","4", 25);
+    G.Insert_Edge("1","5", 45);
+    
+    G.Insert_Edge("2","1", 10);
+    G.Insert_Edge("2","3", 20);
+    G.Insert_Edge("2","4", 25);
+    G.Insert_Edge("2","5", 40);
 
-  
-        
+    G.Insert_Edge("3","1", 55);
+    G.Insert_Edge("3","2", 20);
+    G.Insert_Edge("3","4", 15);
+    G.Insert_Edge("3","5", 30);
+    
+    G.Insert_Edge("4","1", 25);
+    G.Insert_Edge("4","2", 25);
+    G.Insert_Edge("4","3", 15);
+    G.Insert_Edge("4","5", 50);
+    
+    
+    // G.Print();
+    
+    cout<<"El ultimo nodo implica el bucle: es decir apunta al nodo inicial"<<endl;
+    
+    
+    G.heuristicaVecinoMasCercano("1",solucion);
+    printSolution(solucion);
+    
+    // cout<<"En caso de pesos iguales se esta tomanto el nodo mas cercano segun el orden que se ingreso a la lista de vertices"<<endl;
+    // G.heuristicaVecinoMasCercano("5",solucion);
+    // printSolution(solucion);
+    
 }
 
 int main()
