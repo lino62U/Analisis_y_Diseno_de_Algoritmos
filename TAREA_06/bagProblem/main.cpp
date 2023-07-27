5ea04f758e87cd425ad9262e61a511f726f5bdf6
Avelino Lupo <alupoc@unsa.edu.pe>
2023-07-26 19:43:15 -0500

    baf

--- TAREA_06/bagProblem/main.cpp
+++ TAREA_06/bagProblem/main.cpp
@@ -2,38 +2,56 @@
 #include <vector>
 using namespace std;
 
-// A utility function that returns maximum of two integers
 int max(int a, int b) {
     return (a > b) ? a : b;
 }
 
-// Returns the maximum value that can be put in a knapsack of capacity W
+vector<vector<int>> memo; // Tabla de memoización
+
 int knapSack(int W, vector<int>& wt, vector<int>& val, int n) {
-    // Base Case
     if (n == 0 || W == 0)
         return 0;
 
-    // If weight of the nth item is more than Knapsack capacity W, then
-    // this item cannot be included in the optimal solution
-    if (wt[n - 1] > W)
-        return knapSack(W, wt, val, n - 1);
+    if (memo[n][W] != -1)
+        return memo[n][W];
 
-    // Return the maximum of two cases:
-    // (1) nth item included
-    // (2) not included
+    if (wt[n - 1] > W)
+        memo[n][W] = knapSack(W, wt, val, n - 1);
     else
-        return max(
+        memo[n][W] = max(
             val[n - 1] + knapSack(W - wt[n - 1], wt, val, n - 1),
             knapSack(W, wt, val, n - 1)
         );
+
+    return memo[n][W];
 }
 
-// Driver code
 int main() {
-    vector<int> profit = {60, 100, 120};
-    vector<int> weight = {10, 20, 30};
-    int W = 50;
+    vector<int> profit = {1, 2, 3, 4};
+    vector<int> weight = {2, 3, 4, 5};
+    int W = 7;
     int n = profit.size();
-    cout << knapSack(W, weight, profit, n);
+
+    memo.assign(n + 1, vector<int>(W + 1, -1)); // Inicializamos la tabla de memoización con -1
+
+    int maxProfit = knapSack(W, weight, profit, n);
+
+    // Impresión de la tabla de opciones
+    cout << "Tabla de Programación Dinámica:" << endl;
+    for (int i = 0; i <= n; i++) {
+        for (int w = 0; w <= W; w++) {
+            if (i == 0 || w == 0)
+                memo[i][w] = 0;
+            else if (weight[i - 1] <= w)
+                memo[i][w] = max(profit[i - 1] + memo[i - 1][w - weight[i - 1]], memo[i - 1][w]);
+            else
+                memo[i][w] = memo[i - 1][w];
+            cout << memo[i][w] << " ";
+        }
+        cout << endl;
+    }
+
+    cout << "Maximum Profit: " << maxProfit << endl;
+
     return 0;
 }
